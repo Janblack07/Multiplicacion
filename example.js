a5647153487f877dfc4d4d6a584aa9c5f79b7b98
Janblack07 <jandryjacob20@gmail.com>
2023-04-17 14:46:12 -0500

    Se realizo

---
+++ example.js
@@ -0,0 +1,43 @@
+/*  for (let i = 0; i <=12; i++) {
+    console.log("Esta es la tabla del " + i);
+   for (let j = 0; j <= 12; j++) {
+    console.log(i +" * " + j + " = " + (i*j));
+    
+   }
+    
+}
+  */
+/* let i = 0;
+while(i <=12){
+    console.log("Esta es la tabla del " + i);
+    let j=0;
+    while(j<=12){
+        console.log(i +" * " + j + " = " + (i*j));
+        j++;
+    }
+i++;
+} */
+
+/* let i =0;
+do{
+    console.log("Esta es la tabla del " + i);
+    let j=0;
+    do {
+        console.log(i +" * " + j + " = " + (i*j));
+        j++;
+    } while (j<=12);
+    i++;
+}while(i <=12); */
+/* 
+let i = 0;
+let j = 0;
+console.log("Esta es la tabla del " + i);
+while (i < 13) {
+  if (j>12) {
+    i++;
+    console.log("Esta es la tabla del " + i);
+    j = 0;
+  }
+  console.log(i +" * " + j + " = " + (i*j));
+  j++;  
+} */
